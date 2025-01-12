390eee3f951b8308dff29346b0388a59cac49801
rosyiddd666999 <rosidabdul66@gmail.com>
2025-01-12 09:01:55 +0700

    Hai

--- lib/core/configs/themes/app_theme.dart
+++ lib/core/configs/themes/app_theme.dart
@@ -4,66 +4,71 @@ import 'app_colors.dart';
 
 class AppTheme {
   static final lightTheme = ThemeData(
-      primaryColor: AppColors.primary,
-      scaffoldBackgroundColor: AppColors.lightBackground,
-      brightness: Brightness.light,
-      fontFamily: 'Satoshi',
-      sliderTheme:
-          SliderThemeData(overlayShape: SliderComponentShape.noOverlay),
-      inputDecorationTheme: InputDecorationTheme(
-          filled: true,
-          fillColor: Colors.transparent,
-          contentPadding: const EdgeInsets.all(10),
-          hintStyle: const TextStyle(
-            color: Color(0xff383838),
-            fontWeight: FontWeight.w500,
-          ),
-          border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(5),
-              borderSide: const BorderSide(color: Colors.white, width: 0.4)),
-          enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(5),
-              borderSide: const BorderSide(color: Colors.black, width: 0.4))),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              backgroundColor: AppColors.primary,
-              elevation: 0,
-              textStyle:
-                  const TextStyle(fontSize: 16, fontWeight: FontWeight.w900),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(60)))));
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.lightBackground,
+    brightness: Brightness.light,
+    fontFamily: 'Satoshi',
+    sliderTheme: SliderThemeData(overlayShape: SliderComponentShape.noOverlay),
+    inputDecorationTheme: InputDecorationTheme(
+        filled: true,
+        fillColor: Colors.transparent,
+        contentPadding: const EdgeInsets.all(10),
+        hintStyle: const TextStyle(
+          color: Color(0xff383838),
+          fontWeight: FontWeight.w500,
+        ),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5),
+            borderSide: const BorderSide(color: Colors.white, width: 0.4)),
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5),
+            borderSide: const BorderSide(color: Colors.black, width: 0.4))),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        elevation: 0,
+        textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.w900),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(60),
+        ),
+      ),
+    ),
+  );
 
   static final darkTheme = ThemeData(
-      primaryColor: AppColors.primary,
-      scaffoldBackgroundColor: AppColors.darkBackground,
-      brightness: Brightness.dark,
-      fontFamily: 'Satoshi',
-      sliderTheme: SliderThemeData(
-          overlayShape: SliderComponentShape.noOverlay,
-          activeTrackColor: const Color(0xffB7B7B7),
-          inactiveTrackColor: Colors.grey.withOpacity(0.3),
-          thumbColor: const Color(0xffB7B7B7)),
-      inputDecorationTheme: InputDecorationTheme(
-          filled: true,
-          fillColor: AppColors.darkGrey,
-          hintStyle: const TextStyle(
-            color: Color(0xffA7A7A7),
-            fontWeight: FontWeight.w500,
-          ),
-          contentPadding: const EdgeInsets.all(20),
-          border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(5),
-              borderSide: const BorderSide(
-                  color: AppColors.lightBackground, width: 0.4)),
-          enabledBorder: OutlineInputBorder(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.darkBackground,
+    brightness: Brightness.dark,
+    fontFamily: 'Satoshi',
+    sliderTheme: SliderThemeData(
+        overlayShape: SliderComponentShape.noOverlay,
+        activeTrackColor: const Color(0xffB7B7B7),
+        inactiveTrackColor: Colors.grey.withOpacity(0.3),
+        thumbColor: const Color(0xffB7B7B7)),
+    inputDecorationTheme: InputDecorationTheme(
+        filled: true,
+        fillColor: AppColors.darkGrey,
+        hintStyle: const TextStyle(
+          color: Color(0xffA7A7A7),
+          fontWeight: FontWeight.w500,
+        ),
+        contentPadding: const EdgeInsets.all(20),
+        border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(5),
-          )),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              backgroundColor: AppColors.primary,
-              elevation: 0,
-              textStyle:
-                  const TextStyle(fontSize: 16, fontWeight: FontWeight.w900),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(60)))));
+            borderSide:
+                const BorderSide(color: AppColors.lightBackground, width: 0.4)),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+        )),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        elevation: 0,
+        textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.w900),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(60),
+        ),
+      ),
+    ),
+  );
 }
